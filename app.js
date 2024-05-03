c509d23d9127bb9bb2c1c41f8bb9dd6fa6032cd2
Rohit Chouhan <Rohitchouhan201090@gmail.com>
2024-05-03 15:51:44 +0530

    Cors for api fetch

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors'); // Import the cors middleware
 
 // Requiring Routes
 
@@ -21,7 +22,7 @@ require("./Model/db");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
