699250a68139a114e535c15ae683933fa67c239f
Rishi Sharma <rishisharma3034@gmail.com>
2024-05-10 13:40:07 +0530

    For image upload issue

--- utils/fileupload.js
+++ utils/fileupload.js
@@ -3,7 +3,8 @@ const path = require('path');
 const util = require('util')
 const { S3Client } = require('@aws-sdk/client-s3')
 
-const multerS3 = require('multer-s3')
+const multerS3 = require('multer-s3');
+const { log } = require('console');
 
 const s3 = new S3Client({
   credentials: {
@@ -28,35 +29,44 @@ const storage = multerS3({
   bucket: 'androidbucket3577',
   contentType: multerS3.AUTO_CONTENT_TYPE,
   metadata: function (req, file, cb) {
-    cb(null, {fieldName: file.fieldname});
+    cb(null, { fieldName: file.fieldname });
   },
   key: function (req, file, cb) {
     cb(null, Date.now().toString())
   }
 })
-  
 
-  function checkFileType(file, cb) {
-    const filetypes = /jpeg|jpg|png|gif|mp4|mov|png|webp/;
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|gif|webp/; // Removed mp4 and mov
 
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
-    const mimetype = filetypes.test(file.mimetype);
-    
-
-    if (extname && mimetype) {
-      return cb(null, true);
-    } else {
-      cb('Error: Images only (jpeg, jpg, png, gif, mp4, mov, png)!');
+  if (extname) {
+    // Set the MIME type based on the file extension
+    let mimetype = '';
+    if (extname === '.jpeg' || extname === '.jpg') {
+      mimetype = 'image/jpeg';
+    } else if (extname === '.png') {
+      mimetype = 'image/png';
+    } else if (extname === '.gif') {
+      mimetype = 'image/gif';
+    } else if (extname === '.webp') {
+      mimetype = 'image/webp';
     }
+    // Pass the file object with updated MIME type
+    cb(null, true, mimetype);
+  } else {
+    cb('Error: Images only (jpeg, jpg, png, gif, webp)!');
   }
+}
 
-  const upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, cb) {
-      checkFileType(file, cb);
-    },
-  }); 
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
   
   const uploadMiddleWare = upload
 
